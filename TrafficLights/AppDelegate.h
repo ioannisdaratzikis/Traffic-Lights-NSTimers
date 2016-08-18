effdedd1718b9490c01f3effcb32255dcee87c39
geekylemon <aaroncaines@aaron-caines-2>
2016-08-18 15:22:04 +0100

    Initial Commit

---
+++ TrafficLights/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrafficLights
+//
+//  Created by Aaron on 18/08/2016.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrafficLights/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrafficLights
+//
+//  Created by Aaron on 18/08/2016.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
